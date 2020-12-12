7de9422dd3db055d4b74d75fded465b53971a4f6
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-12-12 16:21:02 +0100

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,14 @@
+import datetime
+
+embedcolor = 0x3F79A5
+footer = f"© {datetime.datetime.now().year} - Protectron"
+
+bot_name = "Protectron"
+folder_list = ['core', 'events', 'commands']
+
+host = "HOST"
+user = "USERNAME"
+database = "DATABASE_NAME"
+passwd = "PASSWORD"
+
+token = "PASTE_YOUR_TOKEN_HERE"
